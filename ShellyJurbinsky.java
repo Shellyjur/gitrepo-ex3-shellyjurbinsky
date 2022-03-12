39aafbab161b3c11d0348ee61cefdeb0676a87e8
Shelly <shellyjur@gmail.com>
2022-03-12 22:52:48 +0200

    second commit

--- ShellyJurbinsky.java
+++ ShellyJurbinsky.java
@@ -1,7 +1,7 @@
 public class ShellyJurbinsky{
     public static void main(String[]args){
         for(int i=1;i<=5;i++){
-            System.out.printf("("+i+")"+"Shelly Jurbinsky");
+            System.out.printf(" ("+i+")"+"Shelly Jurbinsky Hello!");
         }
     }
 }
